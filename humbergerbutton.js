08eaff8f5181de86bb434d155b28453855d629e4
enissay-ja <78980526+enissay-ja@users.noreply.github.com>
2022-11-13 21:49:42 +0100

    update

---
+++ humbergerbutton.js
@@ -0,0 +1,6 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0]
+const navbarItems = document.getElementsByClassName("navbar-items")[0]
+
+toggleButton.addEventListener('click',() =>{
+    navbarItems.classList.toggle('active')
+})
